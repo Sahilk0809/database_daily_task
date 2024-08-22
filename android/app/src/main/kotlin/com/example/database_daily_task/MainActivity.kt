81cf220eb035cb63ad6065f627c9790311861d90
Sahil <rk4852115@gmail.com>
2024-08-22 23:37:28 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/database_daily_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.database_daily_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:database_daily_task/view/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => const HomeScreen(),
+        ),
+      ],
+    );
+  }
+}
